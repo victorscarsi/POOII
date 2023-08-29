fca0db5d9a736e440d4d90ddefc14528af3832ee
Victor Scarsi <victorscarsi@gmail.com>
2023-08-28 21:44:27 -0300

    Generics

--- Generics/Generics.java
+++ Generics/Generics.java
@@ -4,7 +4,7 @@ public class Generics {
     public static void main(String[] args) {
         String a = "a";
         String b = "b";
-        System.out.println("Maior entre a e b: " + maior(a, b));
+        System.out.println("Maior entre " + a + " e " + b + ": " + maior(a, b));
     }
 
     private static String maior(String primeira, String segunda) {
